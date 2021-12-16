c42f2f114bdc4dec37f9bc453e835d28cd2038e5
archana-krishna <89037436+archana-krishna@users.noreply.github.com>
2021-12-16 22:59:58 +0530

    first

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OnetempComponent } from './onetemp/onetemp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OnetempComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/onetemp/onetemp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-onetemp',
+  templateUrl: './onetemp.component.html',
+  styleUrls: ['./onetemp.component.css']
+})
+export class OnetempComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
